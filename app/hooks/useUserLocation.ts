51e64765b9339574547d1032ce347714fadb244e
Sam F-Harrison <sgfletcherharrison@gmail.com>
2024-10-21 20:34:12 +0100

    fix(geolocation): make sure user location is ran on the client

--- app/hooks/useUserLocation.ts
+++ app/hooks/useUserLocation.ts
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 interface Location {
   latitude: number;
@@ -16,41 +16,43 @@ const useUserLocation = (): UseUserLocationReturn => {
   const [userLocation, setUserLocation] = useState<Location | null>(null);
   const [locationError, setLocationError] = useState<string | null>(null);
 
-  if ("geolocation" in navigator) {
-    navigator.geolocation.getCurrentPosition(
-      (position: GeolocationPosition) => {
-        const { latitude, longitude } = position.coords;
-        setUserLocation({ latitude, longitude });
-      },
-      (error: GeolocationPositionError) => {
-        switch (error.code) {
-          case error.PERMISSION_DENIED:
-            setLocationError(
-              "Location access has been denied. Please search for a location manually."
-            );
-            break;
-          case error.POSITION_UNAVAILABLE:
-            setLocationError(
-              "Location information is unavailable. Please search for a location manually"
-            );
-            break;
-          case error.TIMEOUT:
-            setLocationError(
-              "We couldn't get your location. Please search for a location manually"
-            );
-            break;
-          default:
-            setLocationError(
-              "An unknown error occurred. Please search for a location manually"
-            );
+  useEffect(() => {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(
+        (position: GeolocationPosition) => {
+          const { latitude, longitude } = position.coords;
+          setUserLocation({ latitude, longitude });
+        },
+        (error: GeolocationPositionError) => {
+          switch (error.code) {
+            case error.PERMISSION_DENIED:
+              setLocationError(
+                "Location access has been denied. Please search for a location manually."
+              );
+              break;
+            case error.POSITION_UNAVAILABLE:
+              setLocationError(
+                "Location information is unavailable. Please search for a location manually"
+              );
+              break;
+            case error.TIMEOUT:
+              setLocationError(
+                "We couldn't get your location. Please search for a location manually"
+              );
+              break;
+            default:
+              setLocationError(
+                "An unknown error occurred. Please search for a location manually"
+              );
+          }
         }
-      }
-    );
-  } else {
-    setLocationError(
-      "Geolocation is not supported by this browser. Please search for a location manually"
-    );
-  }
+      );
+    } else {
+      setLocationError(
+        "Geolocation is not supported by this browser. Please search for a location manually"
+      );
+    }
+  }, []);
 
   return { userLocation, locationError };
 };
